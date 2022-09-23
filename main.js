0825788f6e7a307326b0f24504f5d8439add439c
Francois Schwarzentruber <fschwarz@schumann.irisa.fr>
2022-09-23 18:18:16 +0200

    comments

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-const size = 8;
+const size = 8; //nb of numbers in the input (and the output)
 const signal = [1, 0, 0, 0, 0, 1, 0, 0].map((n) => ({ a: n, b: 0 }));
-let mouse = 0;
+let mouse = { x: 0, y: 0 };
 const SCALE = 128;
 const SCALECOMPLEX = 0.5;
 const CIRCUITSCALE = 0.5;
@@ -12,6 +12,34 @@ const LINEWIDTHBUTTERFLY = 0.01;
 const colors = ["rgb(128, 192, 255)", "cyan", "green", "rgb(0, 192, 0)", "yellow", "orange", "rgb(255, 64, 0)", "rgb(255, 0, 192)", ,];
 
 
+
+/**
+ * 
+ * @param {*} ctx the graphical context (in the rest of code ctx is always the graphical context) 
+ * @param {*} x1 
+ * @param {*} y1 
+ * @param {*} x2 
+ * @param {*} y2 
+ * @description draw a line
+ */
+function line(ctx, x1, y1, x2, y2) {
+    ctx.beginPath();
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.stroke();
+}
+
+
+
+/**
+ * 
+ * @param {*} ctx 
+ * @param {*} x1 
+ * @param {*} x2 
+ * @param {*} ytop 
+ * @param {*} n
+ * @description draw n circuit straight lines 
+ */
 function straightLines(ctx, x1, x2, ytop, n) {
     ctx.strokeStyle = "white";
     for (let w = ytop; w < ytop + n; w++) {
@@ -25,6 +53,15 @@ function straightLines(ctx, x1, x2, ytop, n) {
 }
 
 
+/**
+ * 
+ * @param {*} ctx 
+ * @param {*} x1 
+ * @param {*} x2 
+ * @param {*} ytop 
+ * @param {*} n 
+ * @description draw the reorganisation even VS odd wires
+ */
 function messupLines(ctx, x1, x2, ytop, n) {
     for (let i = 0; i < 2 * n; i++) {
         const i2 = ytop + i
@@ -38,22 +75,32 @@ function messupLines(ctx, x1, x2, ytop, n) {
 }
 
 
-function line(ctx, x1, y1, x2, y2) {
-    ctx.beginPath();
-    ctx.moveTo(x1, y1);
-    ctx.lineTo(x2, y2);
-    ctx.stroke();
-}
 
 
 
-
+/**
+ * 
+ * @param {*} ctx 
+ * @param {*} x 
+ * @param {*} y1 
+ * @param {*} y2
+ * @description draw a FFT butterfly 
+ */
 function butterfly(ctx, x, y1, y2) {
     ctx.lineWidth = LINEWIDTHBUTTERFLY;
     line(ctx, x, y1, x + 0.25, y2);
     line(ctx, x, y2, x + 0.25, y1);
 }
 
+
+/**
+ * 
+ * @param {*} ctx 
+ * @param {*} x 
+ * @param {*} ytop 
+ * @param {*} n
+ * @description draw a collection of FFT butterflies 
+ */
 function butterflies(ctx, x, ytop, n) {
     for (let y = ytop; y < ytop + n; y++) {
         butterfly(ctx, x + CIRCUITSCALE * BUTTERFLIESSCALE * (y - ytop), y, y + n);
@@ -62,25 +109,34 @@ function butterflies(ctx, x, ytop, n) {
         let r = 0.3;
         let omega = { a: r * Math.cos(angle), b: r * Math.sin(angle), color: COLORBUTTERFLY };
 
-        drawComplexArrow(ctx, x + CIRCUITSCALE * BUTTERFLIESSCALE * (y - ytop + 0.4), y  + 0.3, omega)
+        drawComplexArrow(ctx, x + CIRCUITSCALE * BUTTERFLIESSCALE * (y - ytop + 0.4), y + 0.3, omega)
         drawComplexArrow(ctx, x + CIRCUITSCALE * BUTTERFLIESSCALE * (y - ytop + 0.4), y + n - 0.3, oppositeC(omega))
     }
 }
 
 
 
-
+/** manipulation of complex numbers  */
 function addC({ a: a1, b: b1 }, { a: a2, b: b2 }) { return { a: a1 + a2, b: b1 + b2 } };
 function multC({ a: a1, b: b1 }, { a: a2, b: b2, color: color }) { return { a: a1 * a2 - b1 * b2, b: a1 * b2 + a2 * b1, color: color } };
 function oppositeC({ a: a1, b: b1, color: color }) { return { a: -a1, b: -b1, color: color } };
 
-
+/** manipulation of arrays of complex numbers (we represent a "complex number" with an array to keep track on how it is computed ) */
 function add(complexArray1, complexArray2) { return complexArray1.concat(complexArray2) };
 function mult(complex1, complexArray2) { return complexArray2.map(c => multC(complex1, c)) };
 function opposite(complexArray) { return complexArray.map(oppositeC) };
 
 
-
+/**
+ * 
+ * @param {*} ctx 
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} angle 
+ * @param {*} S size of the arrow 
+ * @param {*} A angle of the arrow
+ * @description drawing of an arrow
+ */
 function arrow(ctx, x, y, angle, S = 0.1, A = 0.4) {
     ctx.beginPath();
     ctx.moveTo(x - S * Math.cos(angle - A), y - S * Math.sin(angle - A));
@@ -90,7 +146,15 @@ function arrow(ctx, x, y, angle, S = 0.1, A = 0.4) {
 }
 
 
-
+/**
+ * 
+ * @param {*} ctx 
+ * @param {*} x1 
+ * @param {*} y1 
+ * @param {*} x2 
+ * @param {*} y2 
+ * @description draw of line + arrow
+ */
 function linearrow(ctx, x1, y1, x2, y2) {
     line(ctx, x1, y1, x2, y2);
     const d = Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
@@ -100,12 +164,27 @@ function linearrow(ctx, x1, y1, x2, y2) {
 }
 
 
+/**
+ * 
+ * @param {*} ctx 
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} complex 
+ * @description draw the complex number
+ */
 function drawComplexArrow(ctx, x, y, complex) {
     linearrow(ctx, x, y, x + complex.a * SCALECOMPLEX, y - complex.b * SCALECOMPLEX);
 }
 
 
-
+/**
+ * 
+ * @param {*} ctx 
+ * @param {*} x 
+ * @param {*} y 
+ * @param {*} complexArray 
+ * @description draw an array of complex numbers
+ */
 function drawComplex(ctx, x, y, complexArray) {
     ctx.beginPath();
     const rpoint = 0.05;
@@ -120,8 +199,8 @@ function drawComplex(ctx, x, y, complexArray) {
         if (complexArray[i].a != 0 || complexArray[i].b != 0) {
             ctx.strokeStyle = complexArray[i].color
             drawComplexArrow(ctx, x, y, complexArray[i]);
-            x = x + complexArray[i].a * r;
-            y = y - complexArray[i].b * r;
+            /*   x = x + complexArray[i].a * r;
+               y = y - complexArray[i].b * r;*/
         }
 
     }
@@ -129,24 +208,20 @@ function drawComplex(ctx, x, y, complexArray) {
 
 }
 
-/*
-function drawComplex(ctx, x, y, { a: a, b: b, color: color }) {
-    ctx.beginPath();
-    const rpoint = 0.1;
-    const r = 0.4
-    ctx.fillStyle = "white"
-    ctx.arc(x, y, rpoint, 0, 2 * Math.PI);
-    ctx.fill();
-    ctx.strokeStyle = color ? color : "green"
-
-    ctx.lineWidth = 0.2;
-    line(ctx, x, y, x + a * r, y + b * r);
-    ctx.lineWidth = 0.1;
-}*/
 
 
 
-
+/**
+ * 
+ * @param {*} ctx 
+ * @param {*} x 
+ * @param {*} ytop 
+ * @param {*} n 
+ * @param {*} signal representing as an array of "array of complex numbers"
+ * @param {*} signalLabels e.g. ["a0", "a2", "a4", "a6"]
+ * @returns {x: next right-most x, result: the result of the FFT}
+ * @description draw the FFT circuit
+ */
 function fftcircuit(ctx, x, ytop, n, signal, signalLabels) {
     if (n == 1)
         return { x: x, result: signal };
@@ -227,33 +302,17 @@ function fftcircuit(ctx, x, ytop, n, signal, signalLabels) {
     ctx.stroke();
     ctx.lineWidth = 0.05;
 
-    /*
-    function isPointInRect(p, r) {
-        if (p.x < r.x)
-            return false;
 
-        if (p.x > r.x + r.w)
-            return false;
-
-        if (p.y < r.y)
-            return false;
-
-        if (p.y > r.y + r.h)
-            return false;
-
-        return true;
-    }*/
-
-
-    /*    if (!isPointInRect(mouse, rect))
-            ctx.fill();
-    */
     return { x: xfinal, result: result };
 }
 
 
+/**
+ * 
+ * @param {*} ctx
+ * @description draw the whole circuit 
+ */
 function draw(ctx) {
-
     ctx.clearRect(0, 0, 6040, 4080);
 
 
@@ -261,22 +320,17 @@ function draw(ctx) {
     ctx.font = "0.3px Arial"
     ctx.scale(SCALE, SCALE);
     ctx.lineWidth = 0.05;
-
-
-
     fftcircuit(ctx, CIRCUITSCALE, 1, size, signal.map((c, i) => ([{ a: c.a, b: c.b, color: colors[i] }])), [0, 1, 2, 3, 4, 5, 6, 7].map((i) => "a" + i))
-
-    ctx.fillStyle = "white"
-
-    /*
-    for (let w = 0; w < size; w++)
-        ctx.fillText("a" + w, 0.5, 1 + w + 0.15);
-*/
     ctx.restore();
 }
 
 
-draw(canvas.getContext("2d"));
+draw(canvas.getContext("2d")); //first drawing
+
+
+/**
+ * handle the modification of the input
+ */
 
 let mousePressed = false;
 let wireChanged = undefined;
@@ -288,35 +342,27 @@ canvas.onmousedown = () => {
         const w = Math.round(y);
         if (0 <= w && w <= 7) {
             wireChanged = w;
-            console.log(w)
-
             const ix = (mouse.x - 1) * CIRCUITSCALE;
             const iy = -(y - w);
             signal[w] = { a: ix / SCALECOMPLEX, b: iy / SCALECOMPLEX };
             draw(canvas.getContext("2d"));
         }
     }
-
     mousePressed = true
 }
 canvas.onmouseup = () => { mousePressed = false }
 
-canvas.onclick = () => {
-
-}
 canvas.onmousemove = (evt) => {
     const rect = evt.target.getBoundingClientRect();
     mouse = { x: (evt.clientX - rect.left) / SCALE, y: (evt.clientY - rect.top) / SCALE };
 
     if (mousePressed) {
         if (wireChanged != undefined) {
-
             const y = mouse.y - 1;
             const ix = (mouse.x - 1) * CIRCUITSCALE;
             const iy = -(y - wireChanged);
             signal[wireChanged] = { a: ix / SCALECOMPLEX, b: iy / (SCALECOMPLEX) };
             draw(canvas.getContext("2d"));
-
         }
     }
     draw(canvas.getContext("2d"));
